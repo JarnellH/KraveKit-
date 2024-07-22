60745829989e2399c01f2fe956f02f9c1451a255
JarnellH <jarnell.hayesiii@gmail.com>
2024-07-22 09:07:16 -0700

    Initial commit and Set up of app

---
+++ KraveKit/KraveKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KraveKitApp.swift
+//  KraveKit
+//
+//  Created by Sam Licona on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct KraveKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
